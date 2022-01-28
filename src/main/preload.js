b4e1e9ed657d0c4713275380a105ea3b157228cd
Arkonil <arkonil0904@gmail.com>
2022-01-28 23:05:23 +0530

    initial commit

--- src/main/preload.js
+++ src/main/preload.js
@@ -3,25 +3,7 @@ const mm = require('music-metadata');
 const fs = require('fs');
 
 contextBridge.exposeInMainWorld('electron', {
-  ipcRenderer: {
-    myPing() {
-      ipcRenderer.send('ipc-example', 'ping');
-    },
-    on(channel, func) {
-      const validChannels = ['ipc-example'];
-      if (validChannels.includes(channel)) {
-        // Deliberately strip event as it includes `sender`
-        ipcRenderer.on(channel, (event, ...args) => func(...args));
-      }
-    },
-    once(channel, func) {
-      const validChannels = ['ipc-example'];
-      if (validChannels.includes(channel)) {
-        // Deliberately strip event as it includes `sender`
-        ipcRenderer.once(channel, (event, ...args) => func(...args));
-      }
-    },
-  },
+  ipcRenderer,
   mm,
   fs,
 });
