a3e130bfe743c42576957b44d26419bb56de6765
Arkonil <arkonil0904@gmail.com>
2022-05-10 18:18:56 +0530

    Changed folder name to Player

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,7 +2,7 @@
 // import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 // import icon from '../../assets/icon.svg';
 import './style/App.module.scss';
-import Parent from './components/CurrentPlayer/Parent';
+import Parent from './components/Player/Parent';
 // import Parent from './experiment/Parent';
 
 import ThemeProvider from './components/contexts/ThemeContext';
