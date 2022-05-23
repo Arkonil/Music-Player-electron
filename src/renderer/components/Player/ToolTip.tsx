b8d18ff119b5048ffd836ac38af23789e8c2a77f
Arkonil <arkonil0904@gmail.com>
2022-05-23 15:35:28 +0530

    moved progressbar tooltip to a new file

---
+++ src/renderer/components/Player/ToolTip.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import classes from './PlayerStyles.module.scss';
+import { useTheme } from '../contexts/ThemeContext';
+
+const ToolTip = React.forwardRef(
+  (_props, ref: React.ForwardedRef<HTMLDivElement>) => {
+    const { theme } = useTheme();
+    return (
+      <div className={`player__progress_bar__tooltip ${classes.toolTip}`}>
+        <div ref={ref} hidden>
+          <div
+            style={{
+              backgroundColor: theme.colors.toolTipColor,
+              color: 'black',
+            }}
+          />
+          <div style={{ borderTopColor: theme.colors.toolTipColor }} />
+        </div>
+      </div>
+    );
+  }
+);
+
+export default ToolTip;
