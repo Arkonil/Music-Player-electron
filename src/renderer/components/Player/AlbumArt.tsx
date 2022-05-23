25bef3cb1619ae562aef440ac31c683d985095ea
Arkonil <arkonil0904@gmail.com>
2022-05-23 15:27:19 +0530

    added support for undefined source and custom className

--- src/renderer/components/Player/AlbumArt.tsx
+++ src/renderer/components/Player/AlbumArt.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import image from '../../../../assets/album_art_placeholder.jpg';
+import classes from './PlayerStyles.module.scss';
 
 type AlbumArtProps = {
-  source: string;
-  className: string;
+  source: string | undefined;
+  className?: string;
 };
 
-function AlbumArt({ source, className }: AlbumArtProps) {
-  console.log('Rendering AlbumArt');
+function AlbumArt({ source, className = '' }: AlbumArtProps) {
+  // console.log('Rendering AlbumArt');
   return (
-    <div className={className}>
-      <img src={source || image} alt={image} />
+    <div className={`player__album_art ${classes.albumArt} ${className}`}>
+      <img src={source ?? image} alt={image} />
     </div>
   );
 }
+
+AlbumArt.defaultProps = {
+  className: '',
+};
 
 export default React.memo(AlbumArt);
 // export default AlbumArt;
